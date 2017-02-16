05d9cdea72f4ae120361682e16d19882d1f83488
Shubham Goyal <tango2890@gmail.com>
2017-02-15 20:26:55 -0800

    Both the brainteasers in Leetcode(Filteres using tag)

---
+++ Puzzles/BulbSwitcher319.java
@@ -0,0 +1,11 @@
+package Puzzles;
+
+/**
+ * Created by shubham.goyal on 2/15/17.
+ */
+public class BulbSwitcher319 {
+
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+    }
+}
---
+++ Puzzles/NimGame292.java
@@ -0,0 +1,11 @@
+package Puzzles;
+
+/**
+ * Created by shubham.goyal on 2/15/17.
+ */
+public class NimGame292 {
+
+    public boolean canWinNim(int n) {
+        return !(n % 4 == 0);
+    }
+}
